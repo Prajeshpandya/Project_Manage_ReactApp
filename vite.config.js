174874d71675fb1f9121e374490e965f1fc10a2e
Parth Pandya <211290107020p@gmit.edu.in>
2023-12-28 00:31:05 +0530

    deploy process

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Project_Manage_ReactApp",
+  base: "/Project_Manage_ReactApp/",
   plugins: [react()],
 })
